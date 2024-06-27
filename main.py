bd24d1ab959e99fcabd6e21828e1975ced866287
romeroangel <romeroangel025@gmail.com>
2024-06-27 18:13:31 -0300

    creacion de menu para escoger funcionalidades

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from LectorPDF import LectorPDF
 from EscritorPDF import EscritorPDF
 
-def main(ruta_entrada, ruta_salida):
+def extraer_texto(ruta_entrada, ruta_salida):
     # Leer PDF
     lector_pdf = LectorPDF(ruta_entrada)
     lector_pdf.abrir()
@@ -16,22 +16,39 @@ def main(ruta_entrada, ruta_salida):
         print(f"Texto de la página {i + 1}: {texto}")
         texto_completo += texto + "\n"
 
-    # Escribir PDF
-    escritor_pdf = EscritorPDF(ruta_salida)
-    for i in range(num_paginas):
-        pagina = lector_pdf.obtener_pagina(i)
-        if pagina:
-            escritor_pdf.agregar_pagina(pagina)
-    escritor_pdf.guardar(ruta_salida)
-
-   # En este archivo se guarda el texto 
-    with open("texto_extraido.txt", "w", encoding="utf-8") as archivo_texto:
+    # Opcional: Guardar el texto extraído en un archivo de texto
+    with open(ruta_salida, "w", encoding="utf-8") as archivo_texto:
         archivo_texto.write(texto_completo)
 
-if __name__ == "__main__":
+def unir_pdfs(rutas_entrada, ruta_salida):
+    escritor_pdf = EscritorPDF(ruta_salida)
+    
+    for ruta in rutas_entrada:
+        lector_pdf = LectorPDF(ruta)
+        lector_pdf.abrir()
+        
+        for i in range(lector_pdf.obtener_numero_paginas()):
+            pagina = lector_pdf.obtener_pagina(i)
+            if pagina:
+                escritor_pdf.agregar_pagina(pagina)
+    
+    escritor_pdf.guardar(ruta_salida)
+
+def main():
     parser = argparse.ArgumentParser(description="Procesar archivos PDF.")
-    parser.add_argument("entrada", help="Ruta del archivo PDF de entrada.")
-    parser.add_argument("salida", help="Ruta del archivo PDF de salida.")
+    parser.add_argument("accion", choices=["extraer_texto", "unir_pdfs"], help="Acción a realizar: extraer_texto o unir_pdfs.")
+    parser.add_argument("entradas", nargs='+', help="Rutas de los archivos PDF de entrada.")
+    parser.add_argument("salida", help="Ruta del archivo de salida.")
     args = parser.parse_args()
-    main(args.entrada, args.salida)
+
+    if args.accion == "extraer_texto":
+        if len(args.entradas) != 1:
+            print("Para extraer texto, proporciona una sola ruta de entrada.")
+            return
+        extraer_texto(args.entradas[0], args.salida)
+    elif args.accion == "unir_pdfs":
+        unir_pdfs(args.entradas, args.salida)
+
+if __name__ == "__main__":
+    main()
 
