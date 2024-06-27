c3070119cbd708edc4fb1475be88bec767f8747d
romeroangel <romeroangel025@gmail.com>
2024-06-27 17:33:38 -0300

    modificacion de main para extraer texto

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ def main(ruta_entrada, ruta_salida):
     lector_pdf.abrir()
     num_paginas = lector_pdf.obtener_numero_paginas()
     print(f"Número de páginas: {num_paginas}")
-    texto = lector_pdf.extraer_texto(0)# aca se elije que pagina queres obtener 
-    print(f"Texto de la primera página: {texto}")
+
+    # Extraer texto de todas las páginas
+    texto_completo = ""
+    for i in range(num_paginas):
+        texto = lector_pdf.extraer_texto(i)
+        print(f"Texto de la página {i + 1}: {texto}")
+        texto_completo += texto + "\n"
 
     # Escribir PDF
     escritor_pdf = EscritorPDF(ruta_salida)
-    primera_pagina = lector_pdf.obtener_pagina(0)
-    if primera_pagina:
-        escritor_pdf.agregar_pagina(primera_pagina)
+    for i in range(num_paginas):
+        pagina = lector_pdf.obtener_pagina(i)
+        if pagina:
+            escritor_pdf.agregar_pagina(pagina)
     escritor_pdf.guardar(ruta_salida)
+
+   # En este archivo se guarda el texto 
+    with open("texto_extraido.txt", "w", encoding="utf-8") as archivo_texto:
+        archivo_texto.write(texto_completo)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Procesar archivos PDF.")
